786c54ae082c3a52780ad73add7429684a717065
madstrom2343 <madstrom2343@gmail.com>
2023-06-14 12:03:09 -0700

    First commit to github

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+Console.Title = "Za Warudo";
+Console.ForegroundColor = ConsoleColor.DarkCyan;
+Console.WindowHeight = 22;
+
+// the program begins
+Console.WriteLine("Hello. Who are you?");
+
+// waits for input
+Console.ReadLine();
+
+Console.WriteLine("My name is Za Warudo.\n I am the world but in a different way to say it.");
+Console.WriteLine("What is your favorite color?");
+
+Console.ReadLine();
+
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("Nice, mine is Yellow.");
+
+Console.ReadKey();
